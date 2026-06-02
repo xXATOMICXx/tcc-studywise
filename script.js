76b9dfd64987637ea09a623a27d453f92fb0619d
LeoAraujo2912 <caioaraujo1203@gmail.com>
2026-06-02 20:04:08 -0300

    Update para adicionar interatividade à aba de geração de conteúdo com IA

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-const cartoes = document.querySelectorAll(".cartao");
+const gerar = document.getElementById("cartao_IA");
 
-cartoes.forEach(cartao => {
-    cartao.addEventListener("click", () => {
-        document.body.style.backgroundColor = "#070c21";
-        document.getElementById("conteudo").style.display = "none";
-        document.getElementById("cartoes").style.display = "none";
-    });
+gerar.addEventListener("click", () => {
+    document.getElementById("conteudo").style.display = "none";
+    document.getElementById("cartoes").style.display = "none";
+    document.getElementById("gerar").style.display = "block";
 });
 
 const inicio = document.querySelectorAll(".inicio")
@@ -14,5 +12,6 @@ inicio.forEach( item  => {
         document.body.style.backgroundColor = "#070c21";
         document.getElementById("conteudo").style.display = "block";
         document.getElementById("cartoes").style.display = "flex";
+        document.getElementById("gerar").style.display = "none";
     });
 });
